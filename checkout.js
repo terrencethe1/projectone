9502afd0732fc88073d56bde93da33922f273409
Leon Origer <ljoriger@gmail.com>
2024-10-02 01:09:42 -0500

    Bug fixes, added checkout button functionality

--- checkout.js
+++ checkout.js
@@ -1,8 +1,10 @@
 
 
 //testing - candle array -> local storage, delete later
+
+/*
 let premade = [{"cost": 420, "quantity": 2, "description": "skeletal spine, smells like pomegranates"},
-    {"cost":666, "quantity": 1, "description": "blue votive candle, smells of blueberries and bergamot"},
+    {"cost":666.66, "quantity": 1, "description": "blue votive candle, smells of blueberries and bergamot"},
     {"cost": 1999, "quantity": 45, "description": "tea candle, unscented"} 
 ];
 
@@ -11,16 +13,41 @@ localStorage.setItem('premade', JSON.stringify(premade))};
 
 storeLocalStorage(premade);
 
+
 let custom = [{"cost":3333, "baseScent": "Vanilla", "quantity": 4, "choice1": "coconut", "choice2": "lavender"},
   {"cost":3.14, "baseScent": "Lavender", "quantity": 2, "choice1": "grass", "choice2": "cedarwood", "choice3": "moss"}
 
 
 ];
 
+
 const customLocalStorage = function() {
   localStorage.setItem('custom', JSON.stringify(custom))};
   
   customLocalStorage(custom);
+*/
+  
+
+//no items in cart, just provides error message, should provide link to home - maybe auto redirect?
+// Message displays properly, but code further down throws console error
+// cartTotal function calls line ~133 and ~185 (also the entire checkout button functionality)
+// don't be a fool and push checkout on an empty cart, lets call that one a feature
+
+const cartTable = document.querySelector('#cart-table');
+
+function noCandles () {
+  const noCandles = document.createElement('p');
+  noCandles.textContent = `Your cart is empty!`;
+ cartTable.appendChild(noCandles);
+
+};
+
+if (localStorage.getItem("premade") === null && localStorage.getItem("custom") === null){
+noCandles();
+
+};
+
+
 
 
 //pull candle array(s?) from localstorage, JSON parse into something useful
@@ -31,14 +58,10 @@ custom = JSON.parse(localStorage.getItem("custom"));
 
 
 
-//deal with custom candle array here
-
-
-
 //create table of candle description and cost
 const createCart = function(candleArray) {
 
-    const cartTable = document.querySelector('#cart-table');
+   // const cartTable = document.querySelector('#cart-table');
 
     for (let i = 0; i < candleArray.length; i++) {
         const currentCandle = candleArray[i];
@@ -79,6 +102,7 @@ const createCart = function(candleArray) {
         newTableRow.append(descriptionCell);
     
         const costCell = document.createElement("td");
+        
         // Format the salary as currency
         costCell.textContent = currentCandle.cost.toLocaleString("en-US",{
           style:"currency",
@@ -91,19 +115,108 @@ const createCart = function(candleArray) {
       }
 }  
 
+if (localStorage.getItem("premade") != null){
+createCart(cart) };
 
-createCart(cart);
-createCart(custom);
+if (localStorage.getItem("custom") != null){
+createCart(custom) };
+
+//finalCartTotal is a global variable that cartTotal function will write into
+// will be reused later for checkout modal, declared here to store into
+let finalCartTotal = 0
+
 
 // append summarized total as final row in table
 
 const cartTotal = function(array1, array2){
+  const cartTable = document.querySelector('#cart-table')
+  const newTableRow = document.createElement("tr");
+  let total = 0
+
   for (let i = 0; i < array1.length; i++) {
     const currentCandle = array1[i];
 
     total = total + array1[i].cost
 
-  }
-}
+    
+
+  };
+
+  for (let i = 0; i < array2.length; i++) {
+    const currentCandle = array1[i];
+
+    total = total + array2[i].cost
+
+
+  };
+
+
+  
+  // fills quantity and description table cells with "", would like to remove if time,
+  // a quick google search didn't help - note to self ask Matteo
+
+   const quantityCell = document.createElement("td");
+   quantityCell.textContent = "";
+   newTableRow.append(quantityCell);
+
+  
+   const descriptionCell = document.createElement("td");
+            description = ""
+            newTableRow.append(descriptionCell);
+
+// end total line array fill
+
+ const totalCell = document.createElement("td");
+
+  newTableRow.append(totalCell);
+  totalCell.textContent = finalCartTotal = total.toLocaleString("en-US",{
+    style:"currency",
+    currency:"USD"
+  });
+
+
+  newTableRow.append(totalCell);  
+  cartTable.append(newTableRow);
+
+};
+
+//cartTotal expects two arrays, broke this into cases in case one of them DNE
+//In hindsight there's definitely a more elegant way of handling this mess
+//but I started writing with the plan of doing one array so sorry to anyone else reading this :(
+
+if (localStorage.getItem("premade") === null){
+  cartTotal(custom, 0) 
+  } else if (localStorage.getItem("custom") === null){
+    cartTotal(cart, 0)
+  }else{
+  cartTotal(cart, custom)};
 
 //checkout button does something
+
+const table = document.querySelector("#checkout");
+const checkoutPopup = document.getElementById('checkoutPopup');
+const totalDialog = document.getElementById('total');
+
+  //function for what checkout button does
+  function checkoutConfirm(event){
+
+    totalDialog.innerText = finalCartTotal;
+    
+ 
+// checkout button modal functionality
+// I should implement a choice in popup where one choice doesn't delete arrays,
+//and doesn't redirect to index.html, didn't do it yet
+
+ function closeModal(){
+  localStorage.removeItem('premade');
+  localStorage.removeItem('custom');
+  window.location.href = 'index.html';
+};
+
+
+  checkoutPopup.showModal();
+    const closeButton = document.getElementById("completeCheckout");
+    closeButton.addEventListener("click", closeModal);
+  };
+
+table.addEventListener('click', checkoutConfirm);
